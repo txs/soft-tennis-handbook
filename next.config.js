b6cf3dce51d118c37599ae34eb237ee0e5b2ea5d
txs <txshon@gmail.com>
2024-09-04 06:35:38 +0800

    fix config again and again

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   // ... any other existing configurations
-// }
-
-// module.exports = nextConfig
-
-
 const isProd = process.env.NODE_ENV === 'production'
-const repoName = 'soft-tennis-handbook' // Replace with your repository name
+const repoName = 'soft-tennis-handbook'
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
- 
-// module.exports = withNextra()
 
 module.exports = withNextra({
   output: 'export',
   basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : ''
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  }
 })
- 
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
