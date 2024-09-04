24c335902b7e664a7630071811246115dac382c4
txs <txshon@gmail.com>
2024-09-04 15:43:41 +0800

    Change theme config for site metadata

--- theme.config.jsx
+++ theme.config.jsx
@@ -22,8 +22,31 @@ export default {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="軟式網球入門指南：學習技巧、策略和規則。適合初學者到中級玩家的完整資源。提升你的軟網技能！ | Soft Tennis Handbook: Learn techniques, strategies, and rules. Complete guide for beginners to intermediate players. Improve your soft tennis skills!" />
-      <meta name="og:title" content="軟式網球入門指南 Intro to Soft Tennis" />
     </>
-  )
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 軟式網球入門指南 Intro to Soft Tennis 101 Handbook',
+      description: '軟式網球入門指南：學習技巧、策略和規則。適合初學者到中級玩家的完整資源。提升你的軟網技能！ | Soft Tennis Handbook: Learn techniques, strategies, and rules. Complete guide for beginners to intermediate players. Improve your soft tennis skills!',
+      openGraph: {
+        type: 'website',
+        locale: 'zh_TW',
+        url: 'http://txs.github.io/soft-tennis-handbook/',
+        site_name: '軟式網球入門指南 Intro to Soft Tennis 101 Handbook',
+        images: [
+          {
+            url: 'https://yt3.googleusercontent.com/YaI6jfnglkpz-49wHtyjcgzBJc1MAO1OFOp9WRhx8wV2kctDnDnsgB_tIgbc62JYnhAX8JWZyoA=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj',
+            width: 1707,
+            height: 282,
+            alt: '軟式網球入門指南 Intro to Soft Tennis 101 Handbook',
+          }
+        ],
+      },
+      twitter: {
+        handle: '@txshon',
+        site: '@txshon',
+        cardType: 'summary_large_image',
+      },
+    }
+  }
 }
