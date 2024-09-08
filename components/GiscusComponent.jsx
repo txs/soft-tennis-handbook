ceacf6f87f2fed4b682a558b213a253fbb53d766
txs <txshon@gmail.com>
2024-09-08 08:18:30 +0800

    giscus update based on route change

--- components/GiscusComponent.jsx
+++ components/GiscusComponent.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Giscus from '@giscus/react'
 import { useTheme } from 'next-themes'
+import { useRouter } from 'next/router'
 
 const GiscusComponent = () => {
   const { resolvedTheme } = useTheme()
+  const router = useRouter()
+  const [key, setKey] = useState(0)
+
+  useEffect(() => {
+    // This effect will run on route changes
+    setKey(prevKey => prevKey + 1)
+  }, [router.asPath])
 
   return (
     <Giscus
+      key={key}
       repo="txs/soft-tennis-handbook"
       repoId="R_kgDOMsWGjA"
-      category="Announcements"
+      category="Comments"
       categoryId="DIC_kwDOMsWGjM4CiLWA"
       mapping="og:title"
       strict="0"
       reactionsEnabled="1"
-      emitMetadata="1"
+      emitMetadata="0"
       inputPosition="top"
       theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
       lang="zh-TW"
